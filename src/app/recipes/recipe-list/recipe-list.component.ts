520af5fcd8004898a405204438bba963730fbbc2
CharwekarAkshay <CharwekarAkshay@users.noreply.github.com>
2019-07-01 01:03:43 +0530

    Working on features

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,10 @@ import { Recipe } from '../recipie.modal';
 export class RecipeListComponent implements OnInit {
 
   recipies: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://images.pexels.com/photos/1438672/pexels-photo-1438672.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+    new Recipe('A Test Recipe', 'This is simply a test',
+    'https://images.pexels.com/photos/1438672/pexels-photo-1438672.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+    new Recipe('Test Recipe 2', 'This is again a test recipe',
+    'https://c.pxhere.com/images/7a/68/166bae10efb2ba7e072ca553b907-1589195.jpg!d')
   ];
 
   constructor() { }
